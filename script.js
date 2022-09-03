e4df4bb6e1525d4c72a86d9c1d6b9ed413c8c25a
Gorkasmatzaile <gorkainventor@gmail.com>
2022-09-03 15:38:04 +0200

    Step 2. Create function 'operate'

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ const add = (a,b) => a + b;
 const subtract = (a,b) => a - b;
 const multiply = (a,b) => a * b;
 const divide = (a,b) => b === 0 ? "undefined" : a / b;
+
+// Function for operating
+const operate = (a,operator,b) => {
+    switch(operator) {
+        case '+':
+            return add(a,b);
+        case '-':
+            return subtract(a,b);
+        case '*':
+            return multiply(a,b);
+        case '/':
+            return divide(a,b);
+        default:
+            return "Invalid operator";
+    }
+}
